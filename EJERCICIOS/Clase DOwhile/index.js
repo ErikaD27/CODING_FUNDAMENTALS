59c3365c0d88fd8cf2532856e876cb7a7342e79f
ErikaD27 <erikaduarte2014@hotmail.com>
2022-05-04 22:01:01 -0500

    Rjercicio con dos ciclos(uno dentro del otro)

---
+++ EJERCICIOS/Clase DOwhile/index.js
@@ -0,0 +1,39 @@
+
+
+/*var n = 0;
+var x = 5;
+
+
+
+while (n < x){
+    n = n + 1;
+    console.log("resultado n: ", n)
+}
+console.log(" Ahora ciclo do-while");
+
+var n3 = 1;
+var tabla = 5; */
+/*var imprimeP = "";
+var imprimirHtml = document.getElementById("root");
+
+console.log("element: ", imprimirHtml);
+do{
+    var result = n3 * tabla;
+    console.log("resultado: "+n3+"X"+tabla+"="+result);
+    imprimeP = imprimeP + "<p>"+ n3 + "X" + tabla + "=" + result + "</p>";
+    n3 = n3 + 1;
+}while( n3 < 11);
+
+imprimirHtml.innerHTML = imprimeP;
+
+console.log("element: ", imprimirHtml);*/
+
+var n4 = 1;
+do{
+  
+    for (var index = 1; index < 11; index++){
+        var result = index * n4;
+        console.log("resultado" + index+ "X"+ n4+ "="+result);
+    }
+    n4 = n4 + 1;
+}while( n4 < 11);
---
+++ EJERCICIOS/While.js
@@ -0,0 +1,9 @@
+/*<!--var numeroMenor = 20
+var numeroMayor = 100
+while(numeroMenor<numeroMayor){
+    console.log(numeroMenor)
+    numeroMenor++
+}-->*/
+/*Escribir un script el cual nos deje introducir una cantidad de exámenes y luego introducir la nota de cada exámen de manera individual.
+Al final del programa nos tiene que devolver el promedio de las notas.Condiciones( El usuario no puede ingresar notas negativas, ni mayores a 10 )*/
+
