be8a3ee7b5ba2f7e290fdc01eff5327fd8417a2b
ErikaD27 <erikaduarte2014@hotmail.com>
2022-05-28 15:32:57 -0500

    2

---
+++ EJERCICIOS/Arreglos.js
@@ -0,0 +1,18 @@
+// const originalArray =[1,2,3,4,7,8,9]
+// const prueba =originalArray.splice(3)
+// console.log(prueba)
+
+// for(const numero of[1,2,3,4,5,6,7]){
+//   console.log(numero)
+// }
+
+//FloatingPoints
+let x = 0.3 - 0.2
+let y = 0.2 - 0.1
+
+let xfixed = x.toFixed(2)
+let yfixed = y.toFixed(2)
+
+console.log(x.toFixed(2))
+console.log(y)
+console.log(xfixed===yfixed)
